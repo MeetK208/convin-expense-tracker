41f514b8b40a80eae358e0f43084a7a3b397ee67
meetk <meetkothari208@gmail.com>
2024-10-19 23:33:36 +0530

    db feild changes & created at added

--- expenseTracker/user/models.py
+++ expenseTracker/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from passlib.hash import pbkdf2_sha256
+import datetime
 
 
 class User(models.Model):
@@ -7,9 +8,10 @@ class User(models.Model):
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     mobile_no = models.CharField(max_length=12)
+    created_at = models.DateTimeField(default=datetime.datetime.utcnow)
     
     def verifyPassword(self, raw_password):
         return pbkdf2_sha256.verify(raw_password, self.password)
     
     def __str__(self):
-        return self.user_id + " - " + self.email + " - " +  self.username
+        return str(self.user_id) + " - " + self.email + " - " +  self.username

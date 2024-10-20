c16654ceed54f6810b09e1bf5f582f6443b5bfc0
meetk <meetkothari208@gmail.com>
2024-10-20 22:46:28 +0530

    all installed app added

--- expenseTracker/expenseTracker/settings.py
+++ expenseTracker/expenseTracker/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user'
+    'user',
+    'expense',
+    'balancesheet'
 ]
 
 MIDDLEWARE = [

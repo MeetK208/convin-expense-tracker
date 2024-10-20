742185c6caa8254883d55e713725f6467558de17
meetk <meetkothari208@gmail.com>
2024-10-21 02:36:05 +0530

    view changes for balansheet & pdf generate added

--- expenseTracker/balancesheet/views.py
+++ expenseTracker/balancesheet/views.py
@@ -14,13 +14,82 @@ from django.db import transaction  # Add this at the top of your file
 from expense.models import Expense, Participant, User  # Import your models
 from django.http import HttpResponse
 from utils import generate_report  # Assuming the PDF function is in utils.py
+from django.db.models import Sum
 
 logger = setup_console_logger()
 
 @api_view(['GET'])
 @decorator_from_middleware(AuthenticationMiddleware)
 def overallBalanceSheet(request):
-    return None 
+    try:
+        user_id = request.COOKIES.get('user_id')
+        user = User.objects.get(user_id=user_id)
+        
+        # Get all expenses where the user is a participant
+        participant_expenses = Participant.objects.filter(user=user)
+        
+        # Prepare the response data
+        dashboard_data = {
+            'user_id': user_id,
+            'total_expenses_created': 0.0,
+            'total_owed_by_user': 0.0,
+            'total_paid_by_user': 0.0,
+            'give_expenses': [],
+            'get_expenses': []
+        }
+        
+        for participant in participant_expenses:
+            expense = participant.expense
+            
+            if expense.created_by == user:
+                participants_owed = Participant.objects.filter(expense=expense).exclude(user=user)
+                amount_paid_by_user = participant.amount_paid
+                
+                dashboard_data['total_expenses_created'] += float(expense.total_amount)
+                dashboard_data['total_paid_by_user'] += float(amount_paid_by_user)
+                
+                participants_owes = [{'user_name': p.user.name, 'amount_owed': float(p.amount_owed)} for p in participants_owed]
+                dashboard_data['get_expenses'].append({
+                    'expense_id': expense.expense_id,
+                    'description': expense.description,
+                    'created_by': 'You',
+                    'is_creator': True,
+                    'total_paid': float(amount_paid_by_user),
+                    'owes': participants_owes
+                })
+                
+            else:
+                creator = expense.created_by
+                dashboard_data['total_owed_by_user'] += float(participant.amount_owed)
+                
+                dashboard_data['give_expenses'].append({
+                    'expense_id': expense.expense_id,
+                    'description': expense.description,
+                    'created_by': creator.name,
+                    'is_creator': False,
+                    'total_owed': float(participant.amount_owed),
+                    'give_to': {'user_name': creator.name, 'amount': float(participant.amount_owed)}
+                })
+        
+        download = request.query_params.get('download', 'false').lower() == 'true'
+        if download:
+            return generate_overall_balance_sheet_pdf(dashboard_data)
+        else:
+            return Response(dashboard_data, status=status.HTTP_200_OK)
+    
+    except User.DoesNotExist:
+        return Response({
+            'status': 'error',
+            'message': 'User not found.',
+            'status_code': status.HTTP_404_NOT_FOUND
+        })
+
+    except Exception as e:
+        return Response({
+            'status': 'error',
+            'message': str(e),
+            'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR
+        })
 
 
 @api_view(['GET'])
@@ -95,3 +164,5 @@ def individualBalanceSheet(request):
             'message': str(e),
             'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR
         })
+        
+        
